2e48272605d42c051699df8cdefc6c967eb446ff
yoges <yoges@yogesh>
2024-07-25 14:39:56 -0400

    Hello

--- assignmentweek10/src/application/Main.java
+++ assignmentweek10/src/application/Main.java
@@ -1,240 +1,95 @@
-  package application;
-
-import java.util.logging.Level;
-
-import java.util.logging.Logger;
-
+package application;
 import javafx.application.Application;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler; 
+import javafx.geometry.HPos; 
 import javafx.geometry.Insets;
-
-import javafx.geometry.Pos;
-
-import javafx.stage.Stage;
-
+import javafx.geometry.Pos; 
 import javafx.scene.Scene;
-
-import javafx.scene.control.Alert;
-
-import javafx.scene.control.Alert.AlertType;
-
-import javafx.scene.control.Button;
-
+import javafx.scene.control.Button; 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-
-import javafx.scene.control.PasswordField;
-
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.GridPane;
-
-import javafx.scene.layout.HBox;
-
-import javafx.scene.text.Font;
-
-import javafx.scene.text.FontWeight;
-
-import javafx.scene.text.Text;
-
-
+import javafx.scene.layout.HBox; 
+import javafx.stage.Stage;
 public class Main extends Application {
-
-private static final Logger logger = Logger.getLogger(Main.class.getName());
-
-private UserDao userDao = new UserDao();
-
-@Override
-
-public void start(Stage primaryStage) {
-
-primaryStage.setTitle("JavaFX Welcome");
-
-
-GridPane grid = new GridPane();
-
-grid.setAlignment(Pos.CENTER);
-
-grid.setHgap(10);
-
-grid.setVgap(10);
-
-grid.setPadding(new Insets(25, 25, 25, 25));
-
-
-Text scenetitle = new Text("Add User");
-
-scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-
-grid.add(scenetitle, 0, 0, 2, 1);
-
-
-Label userNameLabel = new Label("Username:");
-
-grid.add(userNameLabel, 0, 1);
-
-
-TextField usernameTextField = new TextField();
-
-grid.add(usernameTextField, 1, 1);
-
-
-Label lastNameLabel = new Label("Last Name:");
-
-grid.add(lastNameLabel, 0, 2);
-
-
-TextField lastNameTextField = new TextField();
-
-grid.add(lastNameTextField, 1, 2);
-
-
-Label firstNameLabel = new Label("First Name:");
-
-grid.add(firstNameLabel, 0, 3);
-
-
-TextField firstNameTextField = new TextField();
-
-grid.add(firstNameTextField, 1, 3);
-
-
- 
-
-
-
-
-Label passwordLabel = new Label("Password:");
-
-grid.add(passwordLabel, 0, 4);
-
-
-PasswordField passwordField = new PasswordField();
-
-grid.add(passwordField, 1, 4);
-
-
-Button saveButton = new Button("Save");
-
-HBox hBox = new HBox(10);
-
-hBox.setAlignment(Pos.BOTTOM_RIGHT);
-
-hBox.getChildren().add(saveButton);
-
-grid.add(hBox, 1, 5);
-
-GridPane pane2 = new GridPane();
-
-pane2.setAlignment(Pos.BOTTOM_RIGHT);
-
-pane2.getChildren().add(saveButton);
-
-PasswordField passwordField2 = new PasswordField();
-
-grid.add(passwordField2, 7, 7);
-
-grid.add(pane2, 6, 6);
-
-saveButton.setOnAction(actionEvent -> {
-
-
-String username = usernameTextField.getText().trim();
-
-String lastName = lastNameTextField.getText().trim();
-
-String firstName = firstNameTextField.getText().trim();
-
-String password = passwordField.getText();
-
-
-if (!StringPool.BLANK.equals(username) && !StringPool.BLANK.equals(lastName)
-
-&& !StringPool.BLANK.equals(firstName) && !StringPool.BLANK.equals(password)) {
-
-try {
-
-if (!userDao.userExists(username)) {
-
-User user = this.createUserObject(username, lastName, firstName, password);
-
-int userId = userDao.saveUser(user);
-
-if (userId > 0) {
-
-this.alert("Save", "Successful!", AlertType.INFORMATION);
-
-} else {
-
-this.alert("Error", "Failed!", AlertType.ERROR);
-
-}
-
-} else {
-
-this.alert("Error", "User already exists!", AlertType.ERROR);
-
-}
-
-} catch (Exception exception) {
-
-logger.log(Level.SEVERE, exception.getMessage());
-
-}
-
-} else {
-
-this.alert("Error", "Please complete fields!", AlertType.ERROR);
-
-}
-
-});
-
-Scene scene = new Scene(grid, 300, 275);
-
-primaryStage.setScene(scene);
-
-primaryStage.show();
-
-}
-
-
-public void alert(String title, String message, AlertType alertType) {
-
-Alert alert = new Alert(alertType);
-
-alert.setTitle(title);
-
-alert.setHeaderText(null);
-
-alert.setContentText(message);
-
-alert.showAndWait();
-
-}
-
-
-public User createUserObject(String username, String lastName, String
-firstName, String password) {
-
-User user = new User();
-
-user.setUsername(username);
-
-user.setLastName(lastName);
-
-user.setFirstName(firstName);
-
-user.setPassword(password);
-
-return user;
-
-}
-
-public static void main(String[] args) {
-
-launch(args);
-
-}
-
-}
-
+	@Override
+	public void start(Stage primaryStage) 
+	{ GridPane pane = new GridPane();
+	pane.setAlignment(Pos.CENTER);
+	pane.setPadding(new Insets(11, 12, 13, 14));
+	pane.setHgap(3); 
+	pane.setVgap(3);
+	pane.setBackground(null);
+	
+	 
+	pane.setStyle("-fx-background-color: orange;");
+	
+	 Label titleLabel = new Label("Student Registration Form");
+     titleLabel.setStyle("-fx-font-weight: bold;");
+     pane.add(titleLabel, 0, 0);
+	
+	pane.add(new Label("Fill in this form to register"), 0, 1);
+	
+	pane.add(new Label("First Name:"), 0, 4);
+	TextField firstNameField = new TextField(); 
+	pane.add(firstNameField, 1, 4);
+	
+	pane.add(new Label("Last Name:"), 0, 5);
+	TextField lastNameField = new TextField();
+	pane.add(lastNameField, 1, 5);
+	
+	pane.add(new Label("Email:"), 0, 6);
+	TextField emailField = new TextField();
+	pane.add(emailField, 1, 6);
+	
+	pane.add(new Label("Date of Birth:"), 0, 7); 
+	DatePicker  datepicker = new DatePicker();
+	pane.add(datepicker, 1, 7); 
+	
+	pane.add(new Label("Set Username:"), 0, 8); 
+	TextField UserNameField = new TextField();
+	pane.add(UserNameField, 1, 8);
+	
+	pane.add(new Label("Set Password:"), 0, 9);
+	TextField PwField = new TextField();
+	pane.add(PwField, 1, 9);
+	
+	pane.add(new Label("Gender:"), 0, 10);
+	ComboBox<String> genderComboBox = new ComboBox<>();
+	genderComboBox.getItems().addAll("Male", "Female", "Other");
+	pane.add(genderComboBox, 1, 10);
+	
+	pane.add(new Label("Course:"), 0, 14);
+	ComboBox<String> courseComboBox = new ComboBox<>();
+	courseComboBox.getItems().addAll("COMP 213", "COMP 930", "COMP 67", "COMP 210", "COMP 105");
+	pane.add(courseComboBox, 1, 14);
+	
+	
+	 
+	
+	HBox buttonBox = new HBox(15);
+	buttonBox.setAlignment(Pos.BOTTOM_RIGHT); 
+	Button btRegister = new Button("Register");
+	OKHandlerClass handler1 = new OKHandlerClass();
+	btRegister.setOnAction(handler1);
+	buttonBox.getChildren().addAll(btRegister);
+	pane.add(buttonBox, 0, 15);
+	GridPane.setHalignment(buttonBox, HPos.CENTER);
+	Scene scene = new Scene(pane, 400, 400); 
+	primaryStage.setScene(scene);
+	primaryStage.setTitle("Week10 Assignment");
+	primaryStage.show();
+	} 
+	public static void main(String[] args) { launch(args);
+	} 
+	} 
+class OKHandlerClass implements EventHandler<ActionEvent> {
+	@Override
+	public void handle(ActionEvent e)
+	{ 
+		System.out.println("Registered!!"); 
+		} 
+	}
